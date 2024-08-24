dc26657f55980f63f47ff44b2a2261246099d9de
abukbt13 <abukbt13@gmail.com>
2024-08-25 01:35:48 +0300

    edit button

--- src/views/TaskListView.vue
+++ src/views/TaskListView.vue
@@ -1,38 +1,69 @@
 
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import axios from "axios";
 
 const title = ref('')
 const description = ref('')
+const status = ref('')
+const tasks = ref(  [])
+
 
 const submitForm =async () => {
   const formData = new FormData();
   formData.append('title', title.value)
   formData.append('description', description.value)
-  const res = await axios.post('tasks', formData)
+  const res = await axios.post('http://localhost:8000/api/v1/tasks', formData)
   if (res.status === 200) {
-    if (res.data.status === 'success') {
-
-    }
+      tasks.value =res.data;
   }
+  status.value =res.data.message
+  getTasks()
 }
+const getTasks = async () => {
+  try {
+    const res = await axios.get('http://localhost:8000/api/v1/tasks', {
+      withCredentials: true // If you need to send cookies or other credentials
+    });
+    tasks.value = res.data.data;
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    // Handle error, e.g., display an error message to the user
+  }
+};
+
+
+
+onMounted(() => {
+  getTasks();
+});
 
 </script>
 
 <template>
-  <div class="task-list container mt-5">
-    <h2>Your Tasks  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+<!--  {{tasks}}-->
+  <div style="width: 600px; background: #ddd;" class="task-list container mt-4 pb-4 ">
+    <h2 class="d-flex justify-content-between p-2">Your Tasks  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
       Add Task
     </button>    </h2>
-    <ul>
-      <li class="">Go wash <i class="bi bi-trash-fill"></i>
+    <hr>
+    <div v-if="status" class="">
+      <div class="p-2 bg-danger text-white">{{status}}</div>
+    </div>
+    <table>
+        <tr>
+          <th></th>
+        </tr>
+    </table>
+    <ul class="list-unstyled" v-for="task in tasks" :task>
+      <li class="d-flex flex-row justify-content-between"> {{ task.title }} <div class="">
+        <i class="bi bi-check-circle-fill"></i><i class="bi bi-pen-fill  ms-3 "></i><i class="bi mx-3 bi-trash-fill"></i></div>
       </li>
-      <li>Go cook </li>
-      <li>Go play </li>
     </ul>
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
       <div class="modal-dialog">
+
+
         <div class="modal-content p-4">
           <h5 class="card-title"> Create task</h5>
           <form @submit.prevent="submitForm">
@@ -41,6 +72,7 @@ const submitForm =async () => {
               <input
                   type="text"
                   id="title"
+                  v-model="title"
                   class="form-control"
                   required
               />
@@ -49,11 +81,12 @@ const submitForm =async () => {
               <label for="description" class="form-label">Description</label>
               <textarea
                   id="description"
+                  v-model="description"
                   class="form-control"
                   required
               ></textarea>
             </div>
-            <button  type="submit" class="btn btn-primary">
+            <button  type="submit" class="btn btn-primary" data-bs-dismiss="modal">
               Create Task
             </button>
             <!--          <router-link to="/tasks" class="btn btn-secondary ms-2">Cancel</router-link>-->
